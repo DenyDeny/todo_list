cf2155ce68e52745bbdabba2cb8918733f8bc87c
Denis Medvedev <oneill222@mail.ru>
2017-11-01 15:19:55 +0500

    Пофиксил config.js

--- src/js/libs/config.js
+++ src/js/libs/config.js
@@ -8,56 +8,56 @@ export default {
         },
 
         login: {
-            url: "/login",
+            url: "/api/user/login",
             method: "post"
         },
 
         logout: {
-            url: "/logout",
+            url: "/api/user/logout",
             method: "post"
         },
 
         register: {
-            url: "/register",
+            url: "/api/user/register",
             method: "post"
         }
     },
 
     userActions: {
         updateInfo: {
-            url: "/update_user_info",
+            url: "/api/userActions/update_user_info",
             method: "post"
         },
 
         getInfo: {
-            url: "/get_user_info",
+            url: "/api/userActions/get_user_info",
             method: "get"
         },
 
         terminateSession: {
-            url: "/terminate_user_session",
+            url: "/api/userActions/terminate_user_session",
             method: "get"
         }
     },
 
     task: {
         list: {
-            url: "/list",
+            url: "/task/list",
             method: "get"
         },
 
         createTask: {
-            url: "/create_task",
+            url: "/task/create_task",
             method: "post"
         },
 
         updateTask: {
-            url: "/update_task",
+            url: "/task/update_task",
             method: "post"
         },
 
         deleteTask: {
-            url: "delete_task",
+            url: "/task/delete_task",
             method: "delete"
         }
     }
